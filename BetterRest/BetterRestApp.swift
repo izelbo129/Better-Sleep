c619706297eb9384fe6bdb14e4ff9b13fce09c1d
Ian Zelbo <113198991+izelbo129@users.noreply.github.com>
2023-08-07 15:44:54 -0400

    Add files via upload

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ian Zelbo on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
